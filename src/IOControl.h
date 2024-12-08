52644dd2d56bcd6b8d3d9feff117620515923c0a
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2024-12-08 15:42:47 +0100

    Update IOControl.h

--- src/IOControl.h
+++ src/IOControl.h
@@ -1,1 +1,22 @@
+#ifndef IOCONTROL_H
+#define IOCONTROL_H
 
+class IOControl
+{
+public:
+	void setPedalName(int pedal, const char* name);
+	void setPedalStatus(int pedal, int status);
+	void setPresetName(const char* name);
+	void setPresetNumber(int number);
+	void setFXStatus(bool status);
+	void setLooperName(int status);
+private:
+	int num[11]; //0-9=pedal status; 10=FX status; 11=Preset number
+	char lcdPedal[10][9]; //10 x LCD 8char screen
+	char lcdPreset[17]; //Preset LCD
+	//char lcdLooper[5]; //Looper LCD
+	const char *lcdLooper; //Looper LCD
+	void pedalUpdate();
+};
+
+#endif
