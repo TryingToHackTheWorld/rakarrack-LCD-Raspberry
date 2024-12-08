b1de569ffddc7b5852a1b70a9800ab1612e78507
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2024-12-08 15:43:28 +0100

    IOControl.C test cout mode

--- src/IOControl.C
+++ src/IOControl.C
@@ -1,1 +1,113 @@
+#include "IOControl.h"
+#include <iostream>
 
+void
+IOControl::setPedalName(int pedal, const char* name)
+{
+	strncpy(lcdPedal[pedal], name, 8);
+	std::cout << "Pedal " << pedal << " - " << lcdPedal[pedal] << "\r\n";
+}
+
+void
+IOControl::setPedalStatus(int pedal, int status)
+{
+	/* 0 = both off
+	*  1 = green
+	*  2 = red
+	*  3 = both on
+	*/
+	num[pedal] = status;
+	std::cout << "Pedal " << pedal << " - ";
+	switch(status)
+	{
+        case 0:
+            std::cout << "OFF";
+            break;
+        case 1:
+            std::cout << "Green";
+            break;
+        case 2:
+            std::cout << "Red";
+            break;
+        case 3:
+            std::cout << "Green+Red";
+            break;
+    }
+	std::cout << "\r\n";
+}
+
+void
+IOControl::setPresetName(const char* name)
+{
+	strncpy(lcdPreset, name, 16);
+	std::cout << "preset " << " - " << lcdPreset << "\r\n";
+}
+
+void
+IOControl::setPresetNumber(int number)
+{
+    num[11] = number;
+    std::cout << "preset number " << " - " << num[11] << "\r\n";
+}
+
+void
+IOControl::setFXStatus(bool status)
+{
+    if(status){
+        num[10] = 1;
+        std::cout << "FX " << " - ON\r\n";
+        pedalUpdate();
+    }else{
+        num[10] = 0;
+        std::cout << "FX " << " - OFF\r\n";
+        pedalUpdate();
+    }
+}
+
+void
+IOControl::setLooperName(int status){
+    /* 0 = Stop
+    *  1 = Reco
+    *  2 = Play
+    *  3 = Paus
+    */
+	switch(status)
+	{
+        case 0:
+            lcdLooper = "STOP";
+            break;
+        case 1:
+            lcdLooper = "RECO";
+            break;
+        case 2:
+            lcdLooper = "PLAY";
+            break;
+        case 3:
+            lcdLooper = "PAUS";
+            break;
+    }
+	std::cout << "Looper - " << lcdLooper << "\r\n";
+}
+
+void
+IOControl::pedalUpdate(){
+	for(int cont=0;cont<10;cont++){
+        if(num[10] == 0){
+            if(num[cont] == 1){
+                setPedalStatus(cont, 0);
+            }else{
+                if(num[cont] == 2){
+                    setPedalStatus(cont, 3);
+                }
+            }
+        }else{
+            if(num[cont] == 0){
+                setPedalStatus(cont, 1);
+            }else{
+                if(num[cont] == 3){
+                    setPedalStatus(cont, 2);
+                }
+            }
+        }
+    }
+}
