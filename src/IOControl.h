bf78ed9f7940da42e4236fed2a75860b3d0d5ec7
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2024-12-11 10:04:58 +0100

    Update IOControl.h

--- src/IOControl.h
+++ src/IOControl.h
@@ -22,7 +22,7 @@ private:
 	int bus1;
 	int bus2;
 	int open_i2c_device(const char * device);
-	void set_target(int bus, uint8_t address, uint16_t target);
+	void send_data(int bus, uint8_t address, uint16_t value);
 };
 
 #endif
