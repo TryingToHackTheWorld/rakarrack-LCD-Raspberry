115d6daf27d18f57e0d47c84e110d525610d9b2c
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-05-20 22:05:46 +0200

    Update IOControl.h

--- src/IOControl.h
+++ src/IOControl.h
@@ -11,18 +11,21 @@ public:
 	void setPresetNumber(int number);
 	void setFXStatus(bool status);
 	void setLooperName(int status);
-	void LCDClean();
 private:
 	int num[11]; //0-9=pedal status; 10=FX status; 11=Preset number
-	char lcdPedal[10][9]; //10 x LCD 8char screen
+	uint8_t address[10]; //Direcciones de los pedales en el bus2
+	char lcdPedal[9]; //Pedal LCD
 	char lcdPreset[17]; //Preset LCD
-	const char *lcdLooper; //Looper LCD
+	char lcdLooper[5]; //Looper LCD
+	void sendTo16LCD(int bus, uint8_t address, char* text);
+	void sendTo8LCD(int bus, uint8_t address, char* text);
+	void sendTo2LED(int bus, uint8_t address, int number);
+	void sendTo4LED(int bus, uint8_t address, char* text);
 	const char *charBus1;
 	const char *charBus2;
 	int bus1;
 	int bus2;
 	int open_i2c_device(const char * device);
-	void sendToI2C(int bus, uint8_t address, char* text);
 };
 
 #endif
