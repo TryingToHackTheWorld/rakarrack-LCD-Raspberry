a2bebdf27e30856e6090d87eee747362fa3fe836
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-05-20 22:05:03 +0200

    Update led0401.h

--- src/led0401.h
+++ src/led0401.h
@@ -1,1 +1,9 @@
+#ifndef LED0401_H
+#define LED0401_H
 
+void init_ht16k33(int i2c_fd, uint8_t address);
+void led0401write(int bus, uint8_t address, char* text);
+extern uint16_t font14seg[128];
+void init_font14seg();
+
+#endif
