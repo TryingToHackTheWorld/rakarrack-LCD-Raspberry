9efe9a45609bd9018dcea61d8509a1803e699891
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-05-23 21:10:26 +0200

    Update led0201.h

--- src/led0201.h
+++ src/led0201.h
@@ -1,8 +1,10 @@
 #ifndef LED0201_H
 #define LED0201_H
 
-void led_0201_init(const char* device, int latch_gpio);
-void led_0201_displayNumber(int number);
+int led_0201_init(const char* spi_device, int latch_gpio);
+void led_0201_show_raw(int value);
+void led_0201_show_number(int number);
 void led_0201_cleanup();
 
 #endif
+
