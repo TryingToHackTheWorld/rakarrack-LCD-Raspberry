0a4b908481476269e6cd3fe7403f3036a0ae4169
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-05-23 16:55:32 +0200

    Update led0201.h

--- src/led0201.h
+++ src/led0201.h
@@ -1,29 +1,8 @@
 #ifndef LED0201_H
 #define LED0201_H
 
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Inicializa el display SPI
-int led_display_init(const char* device);
-
-// Envía un byte al registro del display
-void led_display_send(uint8_t reg, uint8_t data);
-
-// Muestra un número de dos dígitos (00 a 99)
-void led_display_show_number(int value);
-
-// Borra el contenido del display
-void led_display_clear(void);
-
-// Libera recursos y apaga el display
-void led_display_close(void);
-
-#ifdef __cplusplus
-}
-#endif
+void led_0201_init(const char* device, int latch_gpio);
+void led_0201_displayNumber(int number);
+void led_0201_cleanup();
 
 #endif
