9250239abd82680a1dd2b435fd4bfb4027431c76
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-06-04 19:37:45 +0200

    Update lcd1601.h

--- src/lcd1601.h
+++ src/lcd1601.h
@@ -2,8 +2,8 @@
 #define LCD1602_H
 
 void lcd1601WriteString(int i2c_fd, uint8_t address, char* text);
-void lcdWriteString(char *szText);
+void lcd1601WriteString(char *szText);
 void init_lcd_1601(int i2c_fd, uint8_t address);
-static void WriteCommand(unsigned char ucCMD);
+static void WriteCommand1601(unsigned char ucCMD);
 
 #endif
