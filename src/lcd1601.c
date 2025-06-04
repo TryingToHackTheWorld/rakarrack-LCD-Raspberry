71d0d2a4661c9f0700d9b5724ce32b71a98db830
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-06-04 19:37:34 +0200

    Update lcd1601.c

--- src/lcd1601.c
+++ src/lcd1601.c
@@ -47,33 +47,33 @@
 
 #define BACKLIGHT 8
 #define DATA 1
-static int iBackLight = BACKLIGHT;
-static int file_i2c = -1;
+static int iBackLight1601 = BACKLIGHT;
+static int file_i2c_1601 = -1;
 
 void lcd1601WriteString(int i2c_fd, uint8_t address, char* text){
 	char buf[9] = {0};
 	if (ioctl(i2c_fd, I2C_SLAVE, address) < 0) {
         std::cout << "INIT FAIL\r\n";
     }else{
-		file_i2c = i2c_fd;
+		file_i2c_1601 = i2c_fd;
 		
 		// Borrado linea 1
-		WriteCommand(0x80 + 0x00);  // Set cursor to 0x00
-		lcdWriteString("        ");
+		WriteCommand1601(0x80 + 0x00);  // Set cursor to 0x00
+		lcd1601WriteString("        ");
 		
 		// Borrado linea 2
-		WriteCommand(0x80 + 0x40);  // Set cursor to 0x40
-		lcdWriteString("        ");
+		WriteCommand1601(0x80 + 0x40);  // Set cursor to 0x40
+		lcd1601WriteString("        ");
 		
 		// Línea 1 - posición 0x00
 		strncpy(buf, text, 8);
-		WriteCommand(0x80 + 0x00);  // Set cursor to 0x00
-		lcdWriteString(buf);
+		WriteCommand1601(0x80 + 0x00);  // Set cursor to 0x00
+		lcd1601WriteString(buf);
 
 		// Línea lógica 2 - posición 0x40
 		strncpy(buf, text + 8, 8);
-		WriteCommand(0x80 + 0x40);  // Set cursor to 0x40
-		lcdWriteString(buf);
+		WriteCommand1601(0x80 + 0x40);  // Set cursor to 0x40
+		lcd1601WriteString(buf);
 	}
 }
 
@@ -81,67 +81,67 @@ void init_lcd_1601(int i2c_fd, uint8_t address){
 	if (ioctl(i2c_fd, I2C_SLAVE, address) < 0) {
         std::cout << "INIT FAIL\r\n";
     }else{
-		file_i2c = i2c_fd;
+		file_i2c_1601 = i2c_fd;
 		
-		iBackLight = BACKLIGHT; // turn on backlight
+		iBackLight1601 = BACKLIGHT; // turn on backlight
 	
-		WriteCommand(0x02); // Set 4-bit mode of the LCD controller
-		WriteCommand(0x28); // 2 lines, 5x8 dot matrix
-		WriteCommand(0x0c); // display on, cursor off
-		WriteCommand(0x06); // inc cursor to right when writing and don't scroll
-		WriteCommand(0x80); // set cursor to row 1, column 1
-		WriteCommand(0x0E); // clear the screen
-		WriteCommand(0x0C); // display on, cursor off
+		WriteCommand1601(0x02); // Set 4-bit mode of the LCD controller
+		WriteCommand1601(0x28); // 2 lines, 5x8 dot matrix
+		WriteCommand1601(0x0c); // display on, cursor off
+		WriteCommand1601(0x06); // inc cursor to right when writing and don't scroll
+		WriteCommand1601(0x80); // set cursor to row 1, column 1
+		WriteCommand1601(0x0E); // clear the screen
+		WriteCommand1601(0x0C); // display on, cursor off
 	}
 }
 
-static void WriteCommand(unsigned char ucCMD)
+static void WriteCommand1601(unsigned char ucCMD)
 {
 	unsigned char uc;
 
-	uc = (ucCMD & 0xf0) | iBackLight; // most significant nibble sent first
-	write(file_i2c, &uc, 1);
+	uc = (ucCMD & 0xf0) | iBackLight1601; // most significant nibble sent first
+	write(file_i2c_1601, &uc, 1);
 	usleep(PULSE_PERIOD); // manually pulse the clock line
 	uc |= 4; // enable pulse
-	write(file_i2c, &uc, 1);
+	write(file_i2c_1601, &uc, 1);
 	usleep(PULSE_PERIOD);
 	uc &= ~4; // toggle pulse
-	write(file_i2c, &uc, 1);
+	write(file_i2c_1601, &uc, 1);
 	usleep(CMD_PERIOD);
-	uc = iBackLight | (ucCMD << 4); // least significant nibble
-	write(file_i2c, &uc, 1);
+	uc = iBackLight1601 | (ucCMD << 4); // least significant nibble
+	write(file_i2c_1601, &uc, 1);
 	usleep(PULSE_PERIOD);
         uc |= 4; // enable pulse
-        write(file_i2c, &uc, 1);
+        write(file_i2c_1601, &uc, 1);
         usleep(PULSE_PERIOD);
         uc &= ~4; // toggle pulse
-        write(file_i2c, &uc, 1);
+        write(file_i2c_1601, &uc, 1);
 	usleep(CMD_PERIOD);
 }
 
-void lcdWriteString(char *text)
+void lcd1601WriteString(char *text)
 {
 	unsigned char ucTemp[2];
 	int i = 0;
 	
 	while (i<16 && *text)
 	{
-		ucTemp[0] = iBackLight | DATA | (*text & 0xf0);
-		write(file_i2c, ucTemp, 1);
+		ucTemp[0] = iBackLight1601 | DATA | (*text & 0xf0);
+		write(file_i2c_1601, ucTemp, 1);
 		usleep(PULSE_PERIOD);
 		ucTemp[0] |= 4; // pulse E
-		write(file_i2c, ucTemp, 1);
+		write(file_i2c_1601, ucTemp, 1);
 		usleep(PULSE_PERIOD);
 		ucTemp[0] &= ~4;
-		write(file_i2c, ucTemp, 1);
+		write(file_i2c_1601, ucTemp, 1);
 		usleep(PULSE_PERIOD);
-		ucTemp[0] = iBackLight | DATA | (*text << 4);
-		write(file_i2c, ucTemp, 1);
+		ucTemp[0] = iBackLight1601 | DATA | (*text << 4);
+		write(file_i2c_1601, ucTemp, 1);
 		ucTemp[0] |= 4; // pulse E
-                write(file_i2c, ucTemp, 1);
+                write(file_i2c_1601, ucTemp, 1);
                 usleep(PULSE_PERIOD);
                 ucTemp[0] &= ~4;
-                write(file_i2c, ucTemp, 1);
+                write(file_i2c_1601, ucTemp, 1);
                 usleep(CMD_PERIOD);
 		text++;
 		i++;
