debfb5a6c3142edeb766d7665ba2996abac16b35
Trying to hack the world <82749395+TryingToHackTheWorld@users.noreply.github.com>
2025-06-04 22:26:20 +0200

    Update lcd0801.h

--- src/lcd0801.h
+++ src/lcd0801.h
@@ -1,9 +1,7 @@
 #ifndef LCD0801_H
 #define LCD0801_H
 
-#include <stdint.h>
-
-void init_lcd_0801(int bus, uint8_t addr);
-void lcd0801WriteString(int bus, uint8_t addr, const char *str);
+void lcd0801WriteString(int i2c_fd, uint8_t address, char* text);
+void init_lcd_0801(int i2c_fd, uint8_t address);
 
 #endif
